991fb2f567b0447ea938a185c248f6b6f6d1b70a
dam <dam@172.26.39.50>
2019-12-19 14:17:51 +0100

    Cambios 2 19/12/2019

--- src/parking/Principal.java
+++ src/parking/Principal.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package parking;
+import NUMERO.Numero;
+import java.io.IOException;
 
 /**
  *
@@ -13,12 +15,39 @@ public class Principal {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Parking artesano;
+        int opcion;
+        Numero n = new Numero();
         artesano=new Parking(5);//Crea Array. [100]/[X]
         artesano.inicializarPlazas();
+        menu();
+        opcion = n.pedirNumero("", 1, 4);
+        while (opcion != 4){
+            switch (opcion){
+                case 1:
+                    artesano.entrada();
+                    break;
+                case 2:
+                    //artesano.salida();
+                    break;
+                case 3:
+                    artesano.estadoParking();
+                    break;
+            }//Switch
+            menu();
+            opcion = n.pedirNumero("", 1, 4); 
+            }//While
         artesano.estadoParking();
-    }
+    }//main
     
+    public static void menu(){
+        System.out.println("1.ENTRADA");
+        System.out.println("2.SALIDA");
+        System.out.println("3.ESTADO PARKING");
+        System.out.println("4.FIN");
+        System.out.println("OPCION: ");
+    }//Menu
 }
